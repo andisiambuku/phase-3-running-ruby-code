c74fc0c760d11426196d030d26b37dc85afbbde5
andisiambuku <roselandambuku@gmail.com>
2022-10-17 09:54:19 +0300

    Assignment done

---
+++ app.rb
@@ -0,0 +1,7 @@
+#puts displays strings in the terminal
+puts "Hello World!"
+#print dispays strings int he terminal
+print "Pass this test, please."
+#outputs data in the terminal in a better format
+pp [1, 2, 3]
+
